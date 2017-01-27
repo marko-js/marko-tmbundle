6fd05949bd2e1e8f885ada3a9838db4b3d3c4819
Patrick Steele-Idem <pnidem@gmail.com>
2017-01-27 11:26:44 -0700

    Updated grammar

--- update-packages.sh
+++ update-packages.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo ${DIR}/../atom-language-marko/grammars/
 
 # Update the Atom package
 # NOTE: Assumes "../atom-language-marko/" exists and was cloned from https://github.com/marko-js/atom-language-marko
-rm -rf .atom-package
-apm init --package ./.atom-package --convert ./
-rm -rf ../atom-language-marko/grammars/marko.cson
-cp ./.atom-package/grammars/marko.cson ../atom-language-marko/grammars/
+rm -rf ${DIR}/.atom-package
+apm init --package ${DIR}/.atom-package --convert ${DIR}
+rm -rf ${DIR}/../atom-language-marko/grammars/marko.cson
+cp ${DIR}/.atom-package/grammars/marko.cson ${DIR}/../atom-language-marko/grammars/
 
 # Update the Sublime package
 # NOTE: Assumes "../sublime-marko/" exists and was cloned from https://github.com/merwan7/sublime-marko
-rm -rf ../sublime-marko/Syntaxes/Marko.tmLanguage
-cp ./Syntaxes/Marko.tmLanguage ../sublime-marko/Syntaxes/
+# rm -rf ${DIR}/../sublime-marko/Syntaxes/Marko.tmLanguage
+# cp ./Syntaxes/Marko.tmLanguage ${DIR}/../sublime-marko/Syntaxes/
